41e3a3f0b645f5b040178de58204e1aaf7254511
Your Name <you@example.com>
2022-02-05 17:52:47 +0530

    done about component left with images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import Landingabout from "./components/landingabout/Landingabout";
+
 // import About from "./components/about/About";
 
 const App = () => {
   return (
     <>
-        <Navbar />
-        <Home />
-        {/* <About /> */}
-        <Footer />
+      <Navbar />
+      <Home />
+      {/* <About /> */}
+      <Landingabout />
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
