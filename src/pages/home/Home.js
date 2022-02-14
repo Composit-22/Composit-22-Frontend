c8f1f1d22d35c13d7137d2fabb289bc4e31a8156
Utsav <utsavmehta2002@yahoo.com>
2022-02-14 16:09:26 +0530

    Added molecule bkg in register/login, added bkg in about/sponsor.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,21 +6,23 @@ import Gallery from "../../components/gallery/Gallery";
 import Sponsor from "../../components/sponsor/Sponsor";
 import Schedule from "../../components/schedule/Schedule";
 import Landingabout from "../../components/landingabout/Landingabout";
-
+import classes from "./Home.module.css";
 import ScrollContext from "../../store/scroll-context";
 
 const Home = () => {
-    
+
     const scrollCtx = useContext(ScrollContext);
 
     return (
         <>
             <Welcome />
-            <EventCarousel ref={scrollCtx.eventCarouselRef}/>
+            <EventCarousel ref={scrollCtx.eventCarouselRef} />
             <Schedule />
             <Gallery ref={scrollCtx.galleryRef} />
-            <Landingabout />
-            <Sponsor />
+            <div className={classes["combined_bkg"]}>
+                <Landingabout />
+                <Sponsor />
+            </div>
         </>
     );
 };
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,11 +1,13 @@
 import RegisterForm from "../../components/registerform/RegisterForm";
 import classes from "./Register.module.css";
-import registerBkg from "./imgs/register-bkg.png";
 
 const Register = () => {
     return (
-        <div className={classes["register"]}>
-            <RegisterForm />
+        <div className={classes['container']}>
+            <div className={classes['molecule_img']}></div>
+            <div className={classes["register"]}>
+                <RegisterForm />
+            </div>
         </div>
     );
 };
