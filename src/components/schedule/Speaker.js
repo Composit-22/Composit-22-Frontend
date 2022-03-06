5ac3f0800b0a4fce447171f469536589511b4c51
Utsav <utsavmehta2002@yahoo.com>
2022-03-06 10:52:40 +0530

    Completed dark-mode in schedule component.

--- src/components/schedule/Speaker.js
+++ src/components/schedule/Speaker.js
@@ -1,10 +1,13 @@
 import classes from "./Speaker.module.css";
+import {useContext} from "react";
+import DarkContext from '../../store/DarkMode';
 
 const Speaker = (props) => {
+    const { theme } = useContext(DarkContext);
     return (
         <div className={classes["speaker"]}>
             <img src={props.img} alt="Speaker-Img"/>
-            <div className={classes["info"]}>
+            <div className={theme.mode === 'dark' ? `${classes['info']} ${classes['info-dark']}` : classes["info"]}>
                 <span>{props.name}</span>
                 <p>Booth: <strong>{props.booth}</strong></p>
             </div>
