f741c94333f682acafb35671363f8f931eb50c1e
Ritwik Ranjan Mallik <mallikritwik2014@gmail.com>
2022-03-06 15:39:41 +0530

    Add content to our team page

--- src/components/AboutUs/TeamCard.js
+++ src/components/AboutUs/TeamCard.js
@@ -2,40 +2,42 @@ import classes from "./TeamCard.module.css";
 import img from "./dummy.jpg";
 import { useState } from "react";
 const TeamCard = function (props) {
-  const [onState, setOnState] = useState(false);
-  const [offState, setOffState] = useState(true);
-  const enterhoverHandler = function () {
-    setOnState(true);
-    setOffState(false);
-  };
-  const leavehoverHandler = function () {
-    setOnState(false);
-    setOffState(true);
-  };
-  return (
-    <div
-      onMouseOver={enterhoverHandler}
-      onMouseOut={leavehoverHandler}
-      className={`${classes.card}`}
-    >
-      {
-        <img
-          className={`${classes.cardimage}  ${onState ? classes.mouseOn : ""} ${
-            offState ? classes.mouseOff : ""
-          }`}
-          src={img}
-        ></img>
-      }
-      <div
-        className={`${classes.offHoverCardText} ${
-          onState ? classes.cardText : ""
-        }`}
-      >
-        <div>Priyanshu Gautam</div>
-        <br />
-        <div>Frontend</div>
-      </div>
-    </div>
-  );
+    const [onState, setOnState] = useState(false);
+    const [offState, setOffState] = useState(true);
+    const enterhoverHandler = function () {
+        setOnState(true);
+        setOffState(false);
+    };
+    const leavehoverHandler = function () {
+        setOnState(false);
+        setOffState(true);
+    };
+    return (
+        <div
+            onMouseOver={enterhoverHandler}
+            onMouseOut={leavehoverHandler}
+            className={`${classes.card}`}
+        >
+            <div className={`${classes["card-img__container"]}  ${
+                        onState ? classes.mouseOn : ""
+                    } ${offState ? classes.mouseOff : ""}`}>
+                <img
+                    className={`${classes.cardimage}  ${
+                        onState ? classes.mouseOn : ""
+                    } ${offState ? classes.mouseOff : ""}`}
+                    src={props.img}
+                ></img>
+            </div>
+            <div
+                className={`${classes.offHoverCardText} ${
+                    onState ? classes.cardText : ""
+                }`}
+            >
+                <div>{props.name}</div>
+                <br />
+                <div>{props.designation}</div>
+            </div>
+        </div>
+    );
 };
 export default TeamCard;
--- src/pages/ourteam/OurTeam.js
+++ src/pages/ourteam/OurTeam.js
@@ -2,43 +2,118 @@ import TeamCard from "../../components/AboutUs/TeamCard";
 import classes from "./OurTeam.module.css";
 import { useContext } from "react";
 
-import DarkContext from '../../store/DarkMode';
+import DarkContext from "../../store/DarkMode";
+
+import gvjImg from "./imgs/gvj.jpeg";
+import asImg from "./imgs/as.jpg";
+import mbImg from "./imgs/mb.jpg";
+import rrmImg from "./imgs/rrm.png";
+import umImg from "./imgs/um.jpg";
 
 const OurTeam = () => {
-    const {theme} = useContext(DarkContext);
+    const { theme } = useContext(DarkContext);
     return (
-        <div className={theme.mode==='dark'? `${classes['team__container']} ${classes['team__container-dark']}`: classes["team__container"]}>
-        {/* <div className={classes["team__container"]}> */}
+        <div
+            className={
+                theme.mode === "dark"
+                    ? `${classes["team__container"]} ${classes["team__container-dark"]}`
+                    : classes["team__container"]
+            }
+        >
             <div className={classes["team"]}>
-                <h1 className={theme.mode==='dark'? `${classes['team__heading']} ${classes['team__heading-dark']}`: classes["team__heading"]}>
-                {/* <h1 className={classes["team__heading"]}>*/}Web Team</h1> 
-                <ul className={theme.mode==='dark'? `${classes['team__members']} ${classes['team__members-dark']}`: classes["team__members"]}>
-                {/* <ul className={classes["team__members"]}> */}
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
+                <h1
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__heading"]} ${classes["team__heading-dark"]}`
+                            : classes["team__heading"]
+                    }
+                >
+                    Publicity Team
+                </h1>
+                <ul
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__members"]} ${classes["team__members-dark"]}`
+                            : classes["team__members"]
+                    }
+                >
+                    <li className={classes["team__member"]}>
+                        <TeamCard name="Gauri Vignesh Jawalkar" designation="Publicity Head" img={gvjImg} />
+                    </li>
+                    <li className={classes["team__member"]}>
+                        <TeamCard name="Ananay Singh" designation="Publicity Sub Head" img={asImg}/>
+                    </li>
                 </ul>
             </div>
             <div className={classes["team"]}>
-            <h1 className={theme.mode==='dark'? `${classes['team__heading']} ${classes['team__heading-dark']}`: classes["team__heading"]}>
-                {/* <h1 className={classes["team__heading"]}>*/}Graphics Team</h1>
-                <ul className={theme.mode==='dark'? `${classes['team__members']} ${classes['team__members-dark']}`: classes["team__members"]}>
-                {/* <ul className={classes["team__members"]}> */}
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
+                <h1
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__heading"]} ${classes["team__heading-dark"]}`
+                            : classes["team__heading"]
+                    }
+                >
+                    Events Team
+                </h1>
+                <ul
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__members"]} ${classes["team__members-dark"]}`
+                            : classes["team__members"]
+                    }
+                >
+                    <li className={classes["team__member"]}>
+                        <TeamCard name="Maitreyo Biswas" designation="Event Sub Head" img={mbImg}/>
+                    </li>
                 </ul>
             </div>
             <div className={classes["team"]}>
-            <h1 className={theme.mode==='dark'? `${classes['team__heading']} ${classes['team__heading-dark']}`: classes["team__heading"]}>
-                {/* <h1 className={classes["team__heading"]}>*/}Logistics Team</h1>
-            <ul className={theme.mode==='dark'? `${classes['team__members']} ${classes['team__members-dark']}`: classes["team__members"]}>
-                {/* <ul className={classes["team__members"]}> */}
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
-                    <li className={classes["team__member"]}><TeamCard /></li>
+                <h1
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__heading"]} ${classes["team__heading-dark"]}`
+                            : classes["team__heading"]
+                    }
+                >
+                    Web Team
+                </h1>
+                <ul
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__members"]} ${classes["team__members-dark"]}`
+                            : classes["team__members"]
+                    }
+                >
+                    <li className={classes["team__member"]}>
+                        <TeamCard name="Ritwik Ranjan Mallik" designation="Web Sub Head" img={rrmImg} />
+                    </li>
+                    <li className={classes["team__member"]}>
+                        <TeamCard name="Utsav Mehta" designation="Web Sub Head" img={umImg} />
+                    </li>
                 </ul>
             </div>
+            {/* <div className={classes["team"]}>
+                <h1
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__heading"]} ${classes["team__heading-dark"]}`
+                            : classes["team__heading"]
+                    }
+                >
+                    Graphics Team
+                </h1>
+                <ul
+                    className={
+                        theme.mode === "dark"
+                            ? `${classes["team__members"]} ${classes["team__members-dark"]}`
+                            : classes["team__members"]
+                    }
+                >
+                    <li className={classes["team__member"]}>
+                        <TeamCard />
+                    </li>
+                </ul>
+            </div> */}
         </div>
     );
 };
